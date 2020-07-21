62e98e41f00a85329f4481cc4a2b1f95e01d8a14
Bruno B Moraes <bruno_bmoraes@hotmail.com>
2020-07-21 17:30:08 -0300

    ajustes layout capitulo detalhe

--- detonado/views/capitulo.py
+++ detonado/views/capitulo.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView, DetailView
 from detonado.models.capitulo_jogo import CapituloJogoModel
 from detonado.models.jogo import JogoModel
 
+
 class CapituloJogoListView(ListView):
     model = CapituloJogoModel
     template_name = "detonado/capitulo/capitulo-listar.html"
@@ -16,16 +17,13 @@ class CapituloJogoListView(ListView):
         return capitulos
 
 
-
 class CapituloDetail(DetailView):
     model = CapituloJogoModel
     template_name = "detonado/capitulo/capitulo-detalhe.html"
 
     def get_context_data(self, **kwargs):
-        context =super().get_context_data()
+        context = super().get_context_data()
         context['plataformas'] = self.get_object().chave_estrangeira.plataforma_fk.all()
         context['estilos_jogos'] = self.get_object().chave_estrangeira.estitlo_de_jogo_fk.all()
 
         return context
-
-
