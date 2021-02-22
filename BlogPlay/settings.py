1eed20779c166f733611424e422b539cb7870fc6
Bruno B Moraes <bruno_bmoraes@hotmail.com>
2021-02-22 20:36:53 -0300

    nf - deploy  config heroku

--- BlogPlay/settings.py
+++ BlogPlay/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://blog-play.herokuapp.com', 'http://127.0.0.1:8000']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
