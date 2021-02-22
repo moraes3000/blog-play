bee6a57494f790950b411484d321864ffaa5aabe
Bruno B Moraes <bruno_bmoraes@hotmail.com>
2021-02-22 20:26:55 -0300

    nf - deploy  config heroku

--- BlogPlay/settings.py
+++ BlogPlay/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n29ql9o3kiq^r$^y&f^np=)+7p==k6z1$qern7y6l67gcol(5@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://blog-play.herokuapp.com', 'http://127.0.0.1:8000']
 
 # Application definition
 
@@ -81,13 +82,18 @@ WSGI_APPLICATION = 'BlogPlay.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+from dj_database_url import parse as dburl
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # DATABASES = {
 #     'default': {
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn BlogPlay.wsgi --log-file -
